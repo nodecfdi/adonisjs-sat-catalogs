8daf34bd184e46c38c85e8a09868cc77c22da7c9
Fernando Isidro L <luffynando@gmail.com>
2025-12-28 23:31:22 -0600

    build: :construction_worker: update exports with latest generated by tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,11 +3,18 @@ import { defineConfig } from 'tsdown';
 export default defineConfig([
   {
     entry: ['index.ts', 'commands/create_or_update_sat_catalogs_db.ts'],
+    outDir: 'build',
     shims: true,
     format: ['esm'],
     target: 'esnext',
     platform: 'node',
-    exports: true,
+    exports: {
+      customExports(pkg, _context) {
+        pkg['./commands'] = './build/commands/main.js';
+
+        return pkg;
+      },
+    },
     unbundle: true,
   },
 ]);
